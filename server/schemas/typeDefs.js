d17782ed43e651084ec5f45d28d393f56b66ab87
Richard Guarnieri <richard.gm@outlook.com>
2022-08-13 22:55:30 -0400

    added Mutation to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,6 +26,13 @@ const typeDefs = gql`
     type Query {
         me: User
     }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(authors: [String!], description: String!, title: String!, bookId: ID!, image: String!, link: String!): User
+        removeBook(bookId: ID!): User
+    }
 `;
 
 module.exports = typeDefs;
